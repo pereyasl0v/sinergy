6bc04edacd142af8782600fd1ece52ae26d215a3
Pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-06-13 23:17:16 +0300

    Delete 2 semester/python technology stack/Theme_3_Working with data/3.6.while and for loops/3.6.3 directory

--- 2 semester/python technology stack/Theme_3_Working with data/3.6.while and for loops/3.6.3/_2.6.3.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter A number\t"))
-b=int(input("Enter B number\t"))
-
-if a<=b:
-    for i in range(b):
-        if a<b and a%2==0:
-            print(a, end=' ')
-        a += 1;
-        
