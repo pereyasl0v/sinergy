bc4c7743a3728fd38f14de226e7bfe71ddb45a21
Pereyasl0v <121546961+pereyasl0v@users.noreply.github.com>
2023-08-01 16:03:23 +0300

    Add files via upload

---
+++ 2 semester/python technology stack/Theme_3_Working with data/3.8.lists/3.8.1 (corrected)/_3.8.1.py
@@ -0,0 +1,23 @@
+n=int(input("Enter amount of numbers\t"))
+res = []
+check = 0
+i = 0
+while (i < n):
+    j = int(input("Enter number \t"))
+    if (j>=1 and j<=10000):
+        check += j
+        res.append(j)
+        i += 1
+    elif( j> 10000):
+        print("Number > 10e4 , please try again")
+    elif(j < 1):
+        print("Number < 1 , please try again")
+    else:
+        print("No correct value")
+res.reverse()
+
+if check<=100000:
+    for i in range(n):
+        print(res[i], end=' ')
+else:
+    print("Sum number > 10e5")
